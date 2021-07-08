77b8d4ad23478b8ca67e06f1921aef973fa48867
ivirtex <hjozwiakdx@gmail.com>
2021-07-08 09:35:35 +0200

    Change way of pinging 2

--- lib/src/lan_scanner.dart
+++ lib/src/lan_scanner.dart
@@ -1,4 +1,3 @@
-import 'package:dart_ping/dart_ping.dart';
 import 'package:lan_scanner/src/models/device_address.dart';
 import 'dart:async';
 import 'dart:io';
@@ -6,7 +5,6 @@ import 'dart:io';
 /// [LanScanner]
 class LanScanner {
   bool _isScanInProgress = false;
-  List<Future<PingData>> futures = <Future<PingData>>[];
 
   /// Checks if scan is already in progress.
   /// For performance reasons, you can't begin scan while the first one is running
@@ -19,57 +17,53 @@ class LanScanner {
   /// even in the case, they are not online.
   Stream<DeviceAddress> discover({
     required String? subnet,
+    int? port = 80,
     Duration timeout = const Duration(seconds: 5),
     bool verbose = false,
-  }) {
+  }) async* {
     // Check for possible errors in the configuration
-    if (subnet == null) {
-      throw 'Subnet is not set yet';
+    if (subnet == null || port == null) {
+      throw 'Subnet or port is not set yet';
     }
 
     if (_isScanInProgress) {
       throw 'Cannot begin scanning while the first one is still running';
     }
 
-    final controller = StreamController<DeviceAddress>();
-
     for (int addr = 1; addr <= 255; ++addr) {
       final hostToPing = '$subnet.$addr';
 
-      final ping = Ping(hostToPing, count: 1, timeout: timeout.inSeconds);
-      final futurePing = ping.stream.last;
+      try {
+        final Socket socket = await Socket.connect(hostToPing, port);
+        socket.destroy();
 
-      futures.add(futurePing);
-
-      futurePing.then((data) {
-        PingSummary? summary = data.summary;
-
-        if (summary != null) {
-          int received = summary.received;
-
-          if (received > 0) {
-            controller.add(DeviceAddress(
-              exists: true,
-              ip: '$subnet.$addr',
-            ));
-          } else {
-            if (verbose) {
-              controller.add(DeviceAddress(
-                exists: false,
-                ip: '$subnet.$addr',
-              ));
-            }
-          }
+        yield DeviceAddress(exists: true, ip: hostToPing, port: port);
+      } catch (err) {
+        if (!(err is SocketException)) {
+          rethrow;
         }
-      });
+
+        if (err.osError == null ||
+            _errorCodes.contains(err.osError?.errorCode)) {
+          yield DeviceAddress(exists: false, ip: hostToPing, port: port);
+        } else {
+          // Error 23,24: Too many open files in system
+          rethrow;
+        }
+      }
     }
 
-    Future.wait(futures)
-        .then<void>((_) => controller.close())
-        .catchError((_) => controller.close());
-
     _isScanInProgress = false;
-
-    return controller.stream;
   }
+
+  /// 13: Connection failed (OS Error: Permission denied)
+  /// 49: Bind failed (OS Error: Can't assign requested address)
+  /// 61: OS Error: Connection refused
+  /// 64: Connection failed (OS Error: Host is down)
+  /// 65: No route to host
+  /// 101: Network is unreachable
+  /// 111: Connection refused
+  /// 113: No route to host
+  /// <empty>: SocketException: Connection timed out
+  static final _errorCodes = [13, 49, 61, 64, 65, 101, 111, 113];
 }
--- lib/src/models/device_address.dart
+++ lib/src/models/device_address.dart
@@ -2,8 +2,10 @@ class DeviceAddress {
   DeviceAddress({
     required this.exists,
     this.ip,
+    this.port,
   });
 
   final bool exists;
   final String? ip;
+  final int? port;
 }
